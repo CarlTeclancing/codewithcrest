dca96ebe13ee9668f3c336635187ba8a9c46aa46
YuvCarl <carldrake969@gmail.com>
2025-04-16 08:24:21 +0100

    added loading state to data fetch

--- src/components/api/Fetching.jsx
+++ src/components/api/Fetching.jsx
@@ -4,6 +4,8 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 export default function Fetching() {
     const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [errorFetching, setErrorFetching ] = useState('');
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -12,22 +14,39 @@ export default function Fetching() {
                 const json = await response.json();
                 setPosts(json);
                 console.table(json); // Logs the fetched data
+                
             } catch (error) {
                 console.error("Error fetching posts:", error);
+                setErrorFetching(error)
             }
+            setIsLoading(false);
         };
 
         fetchPosts();
+
+
     }, []); // <-- Important: add dependency array to run effect once
 
+    if(isLoading){
+        return <h1>fetching data ...... </h1>
+    }
+
+    if(errorFetching){
+        return <h1>Error Fetching Data!</h1>
+    }
+
     return (
-        <div>
+        <>
             <h2>Fetched Todos</h2>
-            <ul>
+            <div className="post">
                 {posts.map((post) => (
-                    <li key={post.id}>{post.title}</li>
+                    <div key={post.id} className="individual-post">
+                        <h3>{post.title}</h3>
+                        <p>{post.body}</p>
+                        <p>{post.id}</p>
+                    </div>
                 ))}
-            </ul>
-        </div>
+            </div>
+        </>
     );
 }
