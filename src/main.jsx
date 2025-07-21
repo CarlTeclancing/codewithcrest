8aaa730b99bf790a2578dc96cd0fa63db7869510
leslie wafo <leliewafo04@gmail.com>
2025-07-21 15:59:53 +0100

    Layout (sidebar and header), dashboard and ranking page added together with their respective css files.

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Hello from './pages/Hello.jsx'
+<<<<<<< Updated upstream
 
+=======
+import Layout from './components/layout/Layout.jsx'
+import Dashboard from './pages/Dashboard/Dashboard.jsx'
+import Ranking from './pages/ranking/Ranking.jsx'  
+>>>>>>> Stashed changes
 const router = createBrowserRouter([
   {path:'/', element:<App />},
   {path:'/hello', element:<Hello />}
