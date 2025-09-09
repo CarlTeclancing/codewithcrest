9488c469840f8cae96c55c5f268d4967cb7e51e5
Domguia-Simo <domguiasimoulrich@gmail.com>
2025-09-09 16:19:39 +0200

    worked on the submitlab and resources pages

--- src/pages/submitLab/SubmitLab.jsx
+++ src/pages/submitLab/SubmitLab.jsx
@@ -1,10 +1,11 @@
 import React ,{useState ,useEffect} from 'react'
 import './submitlab.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import LabInfo from '../../components/LabInfo.jsx'
 // import Lab from '../../components/Lab.jsx'
 import { HOST_NAME } from '../../../globals.js'
 import {Modal} from 'react-responsive-modal'
+import moduleIcon from '../../assets/module-icon.png'
 
 const lab = [
   { title: 'JavaScript Loops Lab', hashtags:['#Javascript', '#Loops' ,'#Beginner'], task:'Create interactive loops using for while, and for Each methods. Implement counter functionality and array manipulation techniques.' , deadline: 'Due in 2 days', point: '50pts' },
@@ -13,19 +14,23 @@ const lab = [
 
 function SubmitLab() {
 
-  const [labs , setLabs] = useState()
+  const navigate = useNavigate()
+  const [modules , setModules] = useState()
   const [loading ,setLoading] = useState(false)
   const [error ,setError] = useState('')
   const [showModal ,setShowModal] = useState(false)
+  const [confirmModal, setConfirmModal] = useState(false)
 
-  async function getLabs(){
+  async function getModules(){
     try{
       setLoading(true)
       setError('')
-      const response = await fetch(`${HOST_NAME}/api/lab/user/${'user.id'}`)
+      const response = await fetch(`${HOST_NAME}/api/modules/with-labs`)
       const data = await response.json()
+      // console.log(data);
+      
       if(response.ok){
-        // setLabs(data.labs)
+        setModules(data)
       }else{
         setError(data.error)
       }
@@ -40,93 +45,94 @@ function SubmitLab() {
   }
 
   useEffect(()=>{
-    // getLabs()
+    getModules()
   },[0])
+
+  async function startLab(labId){
+    console.log(labId);
+    const file = await fetch(`${HOST_NAME}/labs/ft/html_basics.txt`)
+    if(file.ok){
+      const blob = await file.blob()
+      const url = window.URL.createObjectURL(blob)
+      const link = document.createElement('a')
+      link.href = url
+      link.setAttribute('download', 'html_bascis.txt')
+      document.body.appendChild(link)
+      link.click()
+      link.parentNode.removeChild(link)
+      window.URL.revokeObjectURL(url)
+    }
+    // window.location.href = (`/${HOST_NAME}/labs/ft/html_basics.txt`)
+    return
+    try{
+      const response = await fetch(`${HOST_NAME}/api/lab/start-lab`, {
+        method:'post',
+        headers:{
+          'content-type':'application/json'
+        },
+        body:JSON.stringify({})
+      })
+      const data = await response.json()
+      if(response.ok){
+
+      }else{
+
+      }
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
 
   return (
     <div className='dashboard-container'>
       
       <SubmitLabModal showModal={showModal} setShowModal={setShowModal} />
+      <ConfirmationModal showModal={confirmModal} setShowModal={setConfirmModal} startLab={startLab}/>
 
-      <div style={{lineHeight:'100%'}}>
-        <h3>Active lab</h3>
-        <div className='labs-container'>
-          {lab.slice(0 ,1).map((lab, index) => (
-            <LabInfo
-              key={index}
-              title={lab.title}
-              hashtags={lab.hashtags}
-              task={lab.task}
-              deadline={lab.deadline}
-              point={lab.point}
-              active={true}
-              onClick={()=>setShowModal(true)}
-            />
-          ))}
-        </div>
-      </div>
-
-      <div style={{lineHeight:'100%'}}>
-        <h3>Available labs</h3>
-        <div className='labs-container'>
-          {lab.map((lab, index) => (
-            <LabInfo
-              key={index}
-              title={lab.title}
-              hashtags={lab.hashtags}
-              task={lab.task}
-              deadline={lab.deadline}
-              point={lab.point}
-            />
-          ))}
-        </div>
-      </div>
-
-      
+      {
+        modules && modules.map((mod, idx) => {
+          return(
+              <div key={idx} className={idx != 0? 'module-container module-lock':'module-container'} onClick={()=>{
+                if(idx != 0){
+                  navigate('/subscribe')
+                }
+              }} >
+                <h3 
+                  style={{display:'flex' ,justifyContent:'space-between', alignItems:'center', cursor:idx!=0?'not-allowed':'pointer'}}
+                  // onClick={()=>}
+                > 
+                  <span style={{display:'flex' ,alignItems:'center' ,gap:10}}> 
+                    <img src={moduleIcon} width={20} height={20} /> Module {idx+1} - {mod.name} 
+                  </span> 
+                  
+                  <span style={{border:'solid 0px red' ,marginRight:20 ,padding:'0px 10px'}}>
+                     {/* <i className='bi bi-chevron-down'></i>  */}
+                     <i className={ idx != 0?'bi bi-lock':'bi bi-unlock'}></i> 
+                  </span> 
+                </h3>
 
 
-      {/* <div className="nav">
-        <select name="" id="">
-            <option value="">Show All</option>
-        </select>
-        <div className="bar">
-            <Link>Properties</Link>
-            <Link>Filter</Link>
-            <Link>Sort</Link>
-            <Link>Clear All</Link>
-        </div>
-      </div> */}
+                <div className='labs-container'>
+                  {mod.labs.length != 0 && mod.labs.map((lab, index) => (
+                    <LabInfo
+                      open={false}
+                      key={index}
+                      id={lab.id}
+                      title={lab.title}
+                      onClick={setConfirmModal}
+                      // hashtags={lab.hashtags || []}
+                      task={lab.description || '---'}
+                      // deadline={lab.deadline || 'test'}
+                      // point={lab.point || 5}
+                    />
+                  ))}
+                </div>
 
-      {/* <table>
-        <thead>
-        <tr>
-            <th>Id</th>
-            <th>Lab Title</th>
-            <th>URL</th>
-            <th>Points</th>
-            <th>Date</th>
-            <th>Action</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>01</td>
-            <td>JavaScript Loops Assignment</td>
-            <td className='url'>https://www.producthunt.com</td>
-            <td>832</td>
-            <td>Jan 1, 2020</td>
-            <td>Delivered</td>
-        </tr>
-        <tr>
-            <td>02</td>
-            <td>JavaScript Loops Assignment</td>
-            <td className='url'>https://www.producthunt.com</td>
-            <td>832</td>
-            <td>Jan 1, 2020</td>
-            <td>Delivered</td>
-        </tr>
-        </tbody>
-      </table> */}
+              </div>
+          )
+        })
+      }
 
     </div>
   )
@@ -155,6 +161,24 @@ const SubmitLabModal = ({showModal ,setShowModal}) =>{
             </form>
           </div>
         </p>
+      </Modal>
+    </>
+  )
+}
+
+
+const ConfirmationModal = ({showModal, setShowModal ,startLab}) => {
+    
+  return(
+    <>
+      <Modal open={showModal} onClose={()=>setShowModal(false)} showCloseIcon={false}>
+        <div style={{border:'solid 0px red', textAlign:'center'}}>
+            <h2>Are you sure ?!</h2>
+            <div>
+              <button style={{backgroundColor:'crimson' ,border:'none'}} onClick={()=>setShowModal(false)}>Cancel</button>
+              <button onClick={()=>startLab(showModal)} >Accept</button>
+            </div>
+        </div>
       </Modal>
     </>
   )
